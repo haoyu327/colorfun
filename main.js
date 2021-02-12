b26c5ce9deb35977a34cf27394814c50f3e056b4
haoyuhsu <75198635+haoyu327@users.noreply.github.com>
2021-02-11 18:48:11 -0800

    First Commit

---
+++ main.js
@@ -0,0 +1,15 @@
+function changeColor1() {
+  document.querySelector('#pic').src = '/images/carbg.png'
+}
+function changeColor2() {
+  document.querySelector('#pic').src = '/images/yellowcar.png'
+}
+function changeColor3() {
+  document.querySelector('#pic').src = '/images/purplecar.png'
+}
+function changeColor4() {
+  document.querySelector('#pic').src = '/images/neongreen.png'
+}
+function changeColor5() {
+  document.querySelector('#pic').src = '/images/orangecar.png'
+}
